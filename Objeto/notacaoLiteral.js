707db38bb458a65a23bae974abddafd2a370717c
George Michael Alves <jogos_townsend@hotmail.com>
2020-11-04 00:17:07 -0300

    novo

---
+++ Objeto/notacaoLiteral.js
@@ -0,0 +1,14 @@
+const a = 1
+const b = 2
+const c = 3
+
+const obj1= {a,b,c}
+const obj2 = {a:a, b:b, c:c}
+console.log(obj1,obj2)
+
+const novaNota=10
+const nomeDaPessoa = 'Alfredo'
+
+const obj3 = {}
+obj3[nomeDaPessoa]=novaNota
+console.log(obj3)
